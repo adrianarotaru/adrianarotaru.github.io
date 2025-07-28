98cdd1bc18a05928c6811f9a5132e04e60132098
Adriana Galer Rotaru <42140791+adrianagaler@users.noreply.github.com>
2025-07-27 19:51:10 -0700

    more

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -7,5 +7,13 @@ var addressPoints = [
   [
     "Bruxelles, Belgium",
     50.83874559889915, 4.375116653031177
+  ],
+  [
+    "Chisinau, Moldova",
+    47.018479, 28.843742
+  ],
+  [
+    "Australia",
+    -33.867036, 151.086505
   ]
 ];
